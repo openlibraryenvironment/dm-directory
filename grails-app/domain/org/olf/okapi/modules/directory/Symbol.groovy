076cf0af3ff57dad445b62053d16d6bf46cdf8fb
Ian Ibbotson <ianibbo@gmail.com>
2019-09-21 11:11:22 +0100

    release 1.0.7

--- grails-app/domain/org/olf/okapi/modules/directory/Symbol.groovy
+++ grails-app/domain/org/olf/okapi/modules/directory/Symbol.groovy
@@ -7,7 +7,51 @@ import com.k_int.web.toolkit.tags.Tag
 import grails.gorm.MultiTenant
 import com.k_int.web.toolkit.refdata.RefdataValue;
 import com.k_int.web.toolkit.refdata.Defaults;
+import com.k_int.web.toolkit.databinding.BindUsingWhenRef
+import groovy.util.logging.Log4j
 
+@BindUsingWhenRef({ obj, propName, source ->
+
+  Symbol val = null;
+
+  def data = source.getAt(propName)
+
+  // If the data is asking for null binding then ensure we return here.
+  if (data == null) {
+    return null
+  }
+
+  if ( data instanceof Map ) {
+    if ( data.id ) {
+      val = Symbol.read(data.id);
+    }
+    else if ( ( data.symbol != null ) && ( data.authority != null ) ) {
+
+      def qr = Symbol.executeQuery('select s from Symbol as s where s.symbol=:s and s.authority.symbol=:a',[s:data.symbol, a:data.authority])
+
+      if ( qr.size() == 1 )
+        val = qr.get(0);
+
+      if ( val == null ) {
+        println("Create new directory entry, ${data} - prop=${propName}, source=${source}, source.id=${source?.id}");
+        val = new Symbol()
+        if ( propName == 'symbols' ) {
+          println("Add new directory entry to parent units");
+          source.addToSymbols(val);
+        }
+      }
+    }
+  }
+
+  // Now allow binding of the properties set for that directory entry
+  if (val) {
+    if (data instanceof Map ) {
+      DataBindingUtils.bindObjectToInstance(val, data)
+    }
+  }
+
+  val
+})
 class Symbol  implements MultiTenant<Symbol>  {
 
   String id
