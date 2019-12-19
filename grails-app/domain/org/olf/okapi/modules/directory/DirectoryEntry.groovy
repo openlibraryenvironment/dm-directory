6f9ff837e963ad474a4291df760416c0f0ffccc5
Ian Ibbotson <ianibbo@googlemail.com>
2019-12-19 11:55:42 +0000

    Make DirectoryEntry identifier assigned

--- grails-app/domain/org/olf/okapi/modules/directory/DirectoryEntry.groovy
+++ grails-app/domain/org/olf/okapi/modules/directory/DirectoryEntry.groovy
@@ -70,7 +70,7 @@ class DirectoryEntry  implements MultiTenant<DirectoryEntry>,CustomProperties  {
   ]
 
   static mapping = {
-                 id column:'de_id', generator: 'uuid2', length:36
+                 id column:'de_id', generator: 'assigned', length:36
                name column:'de_name'
                slug column:'de_slug'
         description column:'de_desc'
